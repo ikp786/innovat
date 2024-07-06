1670f094c121bc83908ad7330e203d82f90bb4f5
unknown <mjrp.deepa@gmail.com>
2024-07-06 14:26:47 -0700

    banner height chnage

--- resources/views/front/faq.blade.php
+++ resources/views/front/faq.blade.php
@@ -136,7 +136,7 @@
 			 style="background-image: url('{{ asset('front/upload/files/FAQs.png')}}'); background-size: cover;">
 		<div class="container banner_text pb-60">
 			<!-- Section Content -->
-			<div class="section-content" style="display: none;">
+			<div class="section-content" style="visibility: hidden;">
 				<div class="row">
 					<div class="col-md-6">
 						<h1 class="text-theme-colored2 font-26">Frequently Asked Questions (FAQ) </h1>
